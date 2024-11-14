500b11a2719f853d8028a66e545aedf0278c5ea2
Sedat Uygur <sedat.uygur@outlook.com>
2024-11-14 22:00:54 +0300

    extending get route

--- Twitter/src/twitter.nim
+++ Twitter/src/twitter.nim
@@ -2,9 +2,18 @@ import asyncdispatch, times
 import jester
 import database, views/user, views/index
 
+let db = newDatabase()
 routes:
-  get "/":                            
-    resp renderMain(renderLogin())
+  get "/":
+    if request.cookies.hasKey("username"):
+      var user: User
+      if not db.findUser(request.cookies["username"], user):
+        user = User(username: request.cookies["username"], following: @[])
+        db.create(user)
+      let messages = db.findMessages(user.following)
+      resp renderMain(renderTimeline(user.username, messages))
+    else:
+      resp renderMain(renderLogin())
 
   post "/login":
     setCookie("username", @"username", getTime().getGMTime() + 2.hours)
