9e9f1996912f0637a2089f4126cb738585f1f25f
Sedat Uygur <sedat.uygur@outlook.com>
2024-11-14 22:40:53 +0300

    adding follow button

--- Twitter/src/twitter.nim
+++ Twitter/src/twitter.nim
@@ -2,14 +2,20 @@ import asyncdispatch, times
 import jester
 import database, views/user, views/index
 
+proc userLogin(db: Database, request: Request, user: var User): bool =
+  if request.cookies.hasKey("username"):
+    if not db.findUser(request.cookies["username"], user):
+      user = User(username: request.cookies["username"], following: @[])
+      db.create(user)
+    return true
+  else:
+    return false
+
 let db = newDatabase()
 routes:
   get "/":
-    if request.cookies.hasKey("username"):
-      var user: User
-      if not db.findUser(request.cookies["username"], user):
-        user = User(username: request.cookies["username"], following: @[])
-        db.create(user)
+    var user: User
+    if db.userLogin(request, user):
       let messages = db.findMessages(user.following & user.username)
       resp renderMain(renderTimeline(user.username, messages))
     else:
@@ -20,9 +26,13 @@ routes:
     var user: User
     if not db.findUser(@"name", user):
       halt "User not found"
-
     let messages = db.findMessages(@[user.username])
-    resp renderMain(renderUser(user) & renderMessages(messages))
+
+    var currentUser: User
+    if db.userLogin(request, currentUser):
+      resp renderMain(renderUser(user, currentUser) & renderMessages(messages))
+    else:
+      resp renderMain(renderUser(user) & renderMessages(messages))
 
   post "/login":
     setCookie("username", @"username", getTime().getGMTime() + 2.hours)
