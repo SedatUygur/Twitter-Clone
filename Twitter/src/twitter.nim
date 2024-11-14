23dd9a4da2f363a2c773afbe4187a0d9e9bae250
Sedat Uygur <sedat.uygur@outlook.com>
2024-11-14 22:08:16 +0300

    implementing createMessage route & fixing not appearing tweets

--- Twitter/src/twitter.nim
+++ Twitter/src/twitter.nim
@@ -10,13 +10,22 @@ routes:
       if not db.findUser(request.cookies["username"], user):
         user = User(username: request.cookies["username"], following: @[])
         db.create(user)
-      let messages = db.findMessages(user.following)
+      let messages = db.findMessages(user.following & user.username)
       resp renderMain(renderTimeline(user.username, messages))
     else:
       resp renderMain(renderLogin())
 
   post "/login":
     setCookie("username", @"username", getTime().getGMTime() + 2.hours)
+    redirect("/")
+  
+  post "/createMessage":
+    let message = Message(
+      username: @"username",
+      time: getTime(),
+      msg: @"message"
+    )
+    db.post(message)
     redirect("/")
 
 runForever()
